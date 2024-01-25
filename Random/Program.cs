2edd5360de0e0ee54f9a6a9d634005912bb0fa66
Thodsaphon Sonthiphin <thodsaphonsp@gmail.com>
2024-01-25 11:49:51 +0700

    add random program

---
+++ Random/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+Random random = new();
+
+
+var currentNumber = 1001;
+var stopNumber = 0;
+
+// Continue executing the loop until 'currentNumber' equals to 'stopNumber'
+while (currentNumber != stopNumber)
+{
+    // Generates a random number between 'stopNumber' and 'currentNumber' and assigns it to 'currentNumber'
+    currentNumber = random.Next(stopNumber, currentNumber);
+    
+    // Write the 'currentNumber' to the console
+    Console.WriteLine($"Random number: {currentNumber}");
+}
+
+
